ef17e5c05f3dad41619ad27fed67e2d6749413ca
MeTOR1657 <chokun246810@gmail.com>
2024-12-01 17:15:43 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int num;
+    int e = 0;
+    int o = 0; 
+
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num == 0) {
+            break; 
+        }
+
+        if (num % 2 == 0) {
+            e++;
+        } else {
+            o++;
+        }
+    }
+
+    cout << "#Even numbers = " << e << endl;
+    cout << "#Odd numbers = " << o << endl;
+
     return 0;
 }
